b46a9b8ad69a0f31e1f8e1c03eaaa46cf5b001e8
Doffy_Fastcodingvn <doffy.fastcodingvn@gmail.com>
2023-11-17 18:10:50 +0700

    [fix] folder structure

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import Footer from "./footer.component";
+import Header from "./header.component";
+import Main from "./main.component";
+
+export { Footer, Header, Main }
---
+++ src/pages/home.component.tsx
@@ -0,0 +1,11 @@
+import { Header, Main, Footer } from "../components/index";
+
+export default function Home() {
+  return (
+    <section className="todoapp">
+      <Header />
+      <Main />
+      <Footer />
+    </section>
+  )
+}
